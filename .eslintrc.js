cb4e72d33e3298511df09343c91308fd5d68425e
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-17 09:48:49 +0200

    Update: Enhance through adding `rules` to `.eslintrc.js` file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 module.exports = {
   extends: [
     "next",
@@ -12,5 +12,46 @@ module.exports = {
   ],
   plugins: ["react", "@typescript-eslint"],
   parserOptions: {ecmaVersion: 2021, sourceModule: "module"},
-  rules: {}
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off",
+    "class-methods-use-this": "off",
+    "consistent-return": "off",
+    "func-name-matching": "error",
+    "func-names": "error",
+    "func-style": "error",
+    "no-console": "error",
+    "no-empty-function": "error",
+    "no-nested-ternary": "off",
+    "no-param-reassign": "error",
+    "no-return-await": "off",
+    "no-undef": "error",
+    "no-underscore-dangle": "error",
+    "no-unsafe-negation": "error",
+    "no-unused-vars": ["error", {argsIgnorePattern: "request|response|next"}],
+    "object-shorthand": "error",
+    "prefer-arrow-callback": "error",
+    "prefer-destructuring": "error",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never"
+      }
+    ],
+    "react/jsx-filename-extension": ["error", {extensions: [".ts", ".tsx"]}],
+    "react/jsx-fragments": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/react-in-jsx-scope": "off"
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".ts", ".tsx"]
+      }
+    }
+  }
 }
