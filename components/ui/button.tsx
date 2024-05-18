613ab646c7219ef93a0803e5e925e0c4ab219849
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-18 19:01:11 +0200

    New: Enhance through adding `buttonVariants` to `Button`

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,4 +1,28 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {cva} from "class-variance-authority"
+
+const buttonVariants = cva("shc-button-base", {
+  variants: {
+    variant: {
+      primary: "shc-button-primary",
+      secondary: "shc-button-secondary",
+      outline: "shc-button-outline",
+      link: "shc-button-link"
+    },
+    size: {
+      normal: "shc-button-normal",
+      sm: "shc-button-sm",
+      lg: "shc-button-lg",
+      icon: "shc-button-icon"
+    }
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "normal"
+  }
+})
+
 const Button = function Button() {
   return <button type="button">Button</button>
 }
