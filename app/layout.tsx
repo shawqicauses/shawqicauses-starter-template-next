4e21f83a90101ce53de28397d16fb31edf52b81f
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-18 18:53:43 +0200

    New: Enhance through adding `Button` base CSS styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
+import {Poppins} from "next/font/google"
 import {PropsWithChildren} from "react"
+import "../styles/global.css"
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"]
+})
 
 const Layout = function Layout({children}: PropsWithChildren) {
   return (
     <html lang="en">
       <head />
-      <body>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
