5c1ac6c8e9603a4cc9cd622bd327eee3107e7c13
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-19 23:15:41 +0200

    New: Enhance through adding `FormLabel` types, props, and content

--- components/ui/form/form-label.tsx
+++ components/ui/form/form-label.tsx
@@ -1,6 +1,26 @@
-// DONE REVIEWING: GITHUB COMMIT
-const FormLabel = function FormLabel() {
-  return <div>Form Label</div>
-}
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import * as LabelPrimitive from "@radix-ui/react-label"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+import Label from "../label"
+import {useFormField} from "./form-field"
+
+const FormLabel = forwardRef<
+  ElementRef<typeof LabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+>(({className, ...props}, ref) => {
+  const {error, formItemId} = useFormField()
+  return (
+    <Label
+      ref={ref}
+      htmlFor={formItemId}
+      className={cn(error && "text-red-500", className)}
+      {...props}
+    />
+  )
+})
+
+FormLabel.displayName = "FormLabel"
 
 export default FormLabel
