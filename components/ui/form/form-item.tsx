003b106688a8c85672b4132a8ee32936597e7623
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-19 14:46:44 +0200

    New: Enhance through adding `FormItem` types, context, props, and content
    I forgot to say that I have add `FormField` context in my previous commit

--- components/ui/form/form-item.tsx
+++ components/ui/form/form-item.tsx
@@ -1,6 +1,25 @@
-// DONE REVIEWING: GITHUB COMMIT
-const FormItem = function FormItem() {
-  return <div>Form Item</div>
-}
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {HTMLAttributes, createContext, forwardRef, useId, useMemo} from "react"
+import {cn} from "../../../lib/utils"
+
+type FormItemContextValue = {id: string}
+
+const FormItemContext = createContext<FormItemContextValue>({} as FormItemContextValue)
+
+const FormItem = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({className, ...props}, ref) => {
+    const id = useId()
+    const value = useMemo(() => ({id}), [id])
+
+    return (
+      <FormItemContext.Provider value={value}>
+        <div ref={ref} className={cn("space-y-2", className)} {...props} />
+      </FormItemContext.Provider>
+    )
+  }
+)
+
+FormItem.displayName = "FormItem"
 
 export default FormItem
