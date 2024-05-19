a51b83f6cb1ba0c25cb685e99908254064e0a0e7
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-19 14:34:09 +0200

    New: Enhance through adding `FormField` types, props, and content

--- components/ui/form/form-field.tsx
+++ components/ui/form/form-field.tsx
@@ -1,6 +1,25 @@
-// DONE REVIEWING: GITHUB COMMIT
-const FormField = function FormField() {
-  return <div>Form Field</div>
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {createContext, useMemo} from "react"
+import {Controller, ControllerProps, FieldPath, FieldValues} from "react-hook-form"
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+> = {name: TName}
+
+const FormFieldContext = createContext<FormFieldContextValue>({} as FormFieldContextValue)
+
+const FormField = function FormField<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>
+>({...props}: ControllerProps<TFieldValues, TName>) {
+  const value = useMemo(() => ({name: props.name}), [props.name])
+  return (
+    <FormFieldContext.Provider value={value}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  )
 }
 
 export default FormField
