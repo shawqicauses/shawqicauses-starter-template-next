dee52840d67f0b225ceba4d635c7a49ecf0856a7
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-19 23:01:33 +0200

    New: Enhance through adding `Label` variants, types, props, and content

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,6 +1,19 @@
-// DONE REVIEWING: GITHUB COMMIT
-const Label = function Label() {
-  return <div>Label</div>
-}
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import * as LabelPrimitive from "@radix-ui/react-label"
+import {VariantProps, cva} from "class-variance-authority"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../lib/utils"
+
+const labelVariants = cva("shc-label-base")
+
+const Label = forwardRef<
+  ElementRef<typeof LabelPrimitive.Root>,
+  ComponentPropsWithoutRef<typeof LabelPrimitive.Root> & VariantProps<typeof labelVariants>
+>(({className, ...props}, ref) => {
+  return <LabelPrimitive.Root ref={ref} className={cn(labelVariants(), className)} {...props} />
+})
+
+Label.displayName = LabelPrimitive.Root.displayName
 
 export default Label
