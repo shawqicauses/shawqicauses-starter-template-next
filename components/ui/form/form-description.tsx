57f3b845b768d1cab0c110e300749a3dbc5304c7
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-20 16:32:18 +0200

    New: Enhance through adding `FormDescription` types, props, and content

--- components/ui/form/form-description.tsx
+++ components/ui/form/form-description.tsx
@@ -1,6 +1,23 @@
-// DONE REVIEWING: GITHUB COMMIT
-const FormDescription = function FormDescription() {
-  return <div>Form Description</div>
-}
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {HTMLAttributes, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+import {useFormField} from "./form-field"
+
+const FormDescription = forwardRef<HTMLParagraphElement, HTMLAttributes<HTMLParagraphElement>>(
+  ({className, ...props}, ref) => {
+    const {formDescriptionId} = useFormField()
+    return (
+      <p
+        id={formDescriptionId}
+        ref={ref}
+        className={cn("text-sm text-muted-foreground", className)}
+        {...props}
+      />
+    )
+  }
+)
+
+FormDescription.displayName = "FormDescription"
 
 export default FormDescription
