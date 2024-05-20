40551915eb899c3dd0c3d38c565bff2190f700fc
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-05-20 16:27:42 +0200

    New: Enhance through adding `FormControl` types, props, and content

--- components/ui/form/form-control.tsx
+++ components/ui/form/form-control.tsx
@@ -1,6 +1,24 @@
-// DONE REVIEWING: GITHUB COMMIT
-const FormControl = function FormControl() {
-  return <div>Form Control</div>
-}
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+
+import {Slot} from "@radix-ui/react-slot"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {useFormField} from "./form-field"
+
+const FormControl = forwardRef<ElementRef<typeof Slot>, ComponentPropsWithoutRef<typeof Slot>>(
+  ({...props}, ref) => {
+    const {error, formItemId, formDescriptionId, formMessageId} = useFormField()
+    return (
+      <Slot
+        id={formItemId}
+        ref={ref}
+        aria-invalid={!!error}
+        aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+        {...props}
+      />
+    )
+  }
+)
+
+FormControl.displayName = "FormControl"
 
 export default FormControl
