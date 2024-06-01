8dff733f570f247d584e6ee3ff9a817b8e6ce3e2
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-01 07:21:03 +0200

    New: Enhance through adding `fetchRetry` to `shcFetch`

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,4 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT 4️⃣
+// DONE REVIEWING: GITHUB COMMIT 5️⃣
 
 type JSONValue = string | number | boolean | {[key: string]: JSONValue} | JSONValue[]
 
@@ -6,7 +6,6 @@ type FetchOptionsBase = {
   method?: "GET" | "POST" | "PUT" | "DELETE"
   headers?: Record<string, string>
   timeout?: number
-  retries?: number
 }
 
 type FetchOptionsWithBody<T> = FetchOptionsBase & {
@@ -20,6 +19,10 @@ type FetchOptionsWithoutBody = FetchOptionsBase & {
 }
 
 type FetchOptions<T> = FetchOptionsWithBody<T> | FetchOptionsWithoutBody
+
+type SHCRequestInit = RequestInit & {
+  timeout?: number
+}
 
 class FetchError extends Error {
   public status: number
@@ -64,6 +67,10 @@ const ERRORS_DETAILS: Record<string | number, {statusText: string; message: stri
     statusText: "INTERNAL_SERVER_ERROR",
     message: "There was an error on our server. Please try again later."
   },
+  retries: {
+    statusText: "MAXIMUM_RETRIES",
+    message: "Maximum retries reached. Please try again."
+  },
   default: {
     statusText: "ERROR",
     message: "An un-expected error occurred. Please try again."
@@ -72,7 +79,7 @@ const ERRORS_DETAILS: Record<string | number, {statusText: string; message: stri
 
 const fetchWithTimeout = function fetchWithTimeout(
   url: string,
-  options: RequestInit & {timeout?: number}
+  options: SHCRequestInit
 ): Promise<Response> {
   const {timeout = 8000, ...optionsRest} = options
 
@@ -93,6 +100,35 @@ const fetchWithTimeout = function fetchWithTimeout(
         reject(new FetchError(0, statusText, message, error))
       })
   })
+}
+
+const fetchRetry = async function fetchRetry(
+  url: string,
+  options: SHCRequestInit,
+  retries: number = 3
+): Promise<Response> {
+  try {
+    const response = await fetchWithTimeout(url, options)
+    if (!response.ok) {
+      const {statusText, message} = ERRORS_DETAILS[response.status] || ERRORS_DETAILS.default
+      throw new FetchError(response.status, statusText, message)
+    }
+
+    return response
+  } catch (error) {
+    if (error instanceof FetchError && error.status === 408 && retries > 1)
+      return fetchRetry(url, options, retries - 1)
+    if (retries <= 1) {
+      const {statusText, message} = ERRORS_DETAILS.retries
+      throw new FetchError(408, statusText, message, error as Error)
+    }
+
+    if (error instanceof FetchError)
+      throw new FetchError(error.status, error.statusText, error.message, error)
+
+    const {statusText, message} = ERRORS_DETAILS.default
+    throw new FetchError(0, statusText, message, error as Error)
+  }
 }
 
 export const shcFetch = function shcFetch() {
