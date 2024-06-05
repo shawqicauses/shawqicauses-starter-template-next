c6a7424dd11927af5369c9ac1f91949934adbf1a
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-05 13:13:02 +0200

    New: Enhance through adding `AlertDialogHeader`

---
+++ components/ui/alert-dialog/alert-dialog-header.tsx
@@ -0,0 +1,15 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {HTMLAttributes} from "react"
+import {cn} from "../../../lib/utils"
+
+const AlertDialogHeader = function AlertDialogHeader({
+  className,
+  ...props
+}: HTMLAttributes<HTMLDivElement>) {
+  return (
+    <div className={cn("flex flex-col space-y-2 text-center sm:text-left", className)} {...props} />
+  )
+}
+
+AlertDialogHeader.displayName = "AlertDialogHeader"
+export default AlertDialogHeader
