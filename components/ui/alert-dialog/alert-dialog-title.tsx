5996fcdfe29e31a3620ed5d8c7c9cf68a6ca00d0
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-05 13:25:14 +0200

    New: Enhance through adding `AlertDialogTitle`

---
+++ components/ui/alert-dialog/alert-dialog-title.tsx
@@ -0,0 +1,18 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const AlertDialogTitle = forwardRef<
+  ElementRef<typeof AlertDialogPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Title>
+>(({className, ...props}, ref) => (
+  <AlertDialogPrimitive.Title
+    ref={ref}
+    className={cn("text-lg font-semi-bold", className)}
+    {...props}
+  />
+))
+
+AlertDialogTitle.displayName = "AlertDialogTitle"
+export default AlertDialogTitle
