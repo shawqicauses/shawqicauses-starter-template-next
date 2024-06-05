583f20e480e35add886bf7ddd7f7b90c00ff6dac
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-05 14:08:23 +0200

    New: Enhance through adding `AlertDialogAction`

---
+++ components/ui/alert-dialog/alert-dialog-action.tsx
@@ -0,0 +1,15 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+import {buttonVariants} from "../button"
+
+const AlertDialogAction = forwardRef<
+  ElementRef<typeof AlertDialogPrimitive.Action>,
+  ComponentPropsWithoutRef<typeof AlertDialogPrimitive.Action>
+>(({className, ...props}, ref) => (
+  <AlertDialogPrimitive.Action ref={ref} className={cn(buttonVariants(), className)} {...props} />
+))
+
+AlertDialogAction.displayName = "AlertDialogAction"
+export default AlertDialogAction
