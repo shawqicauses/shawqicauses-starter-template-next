968a17c00fd59b18e83ab7a50cc675acde3d459d
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-05 14:28:01 +0200

    New: Enhance through adding `AlertDialogTrigger`

--- components/ui/alert-dialog/index.tsx
+++ components/ui/alert-dialog/index.tsx
@@ -1,7 +1,7 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
 import * as AlertDialogPrimitive from "@radix-ui/react-alert-dialog"
 
 const AlertDialog = AlertDialogPrimitive.Root
 export const AlertDialogPortal = AlertDialogPrimitive.Portal
-
+export const AlertDialogTrigger = AlertDialogPrimitive.Trigger
 export default AlertDialog
