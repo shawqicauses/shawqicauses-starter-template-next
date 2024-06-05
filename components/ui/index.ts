2c7def719a42947c8a2c8a8620878b83d409d069
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-05 14:36:57 +0200

    Update: Enhance through updating `export` in `ui/index.ts`

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,5 +1,13 @@
-// DONE REVIEWING: GITHUB COMMIT 1️⃣
+// DONE REVIEWING: GITHUB COMMIT 2️⃣
 export {default as AlertDialog} from "./alert-dialog"
+export {default as AlertDialogAction} from "./alert-dialog/alert-dialog-action"
+export {default as AlertDialogCancel} from "./alert-dialog/alert-dialog-cancel"
+export {default as AlertDialogContent} from "./alert-dialog/alert-dialog-content"
+export {default as AlertDialogDescription} from "./alert-dialog/alert-dialog-description"
+export {default as AlertDialogFooter} from "./alert-dialog/alert-dialog-footer"
+export {default as AlertDialogHeader} from "./alert-dialog/alert-dialog-header"
+export {default as AlertDialogOverlay} from "./alert-dialog/alert-dialog-overlay"
+export {default as AlertDialogTitle} from "./alert-dialog/alert-dialog-title"
 export {default as Button} from "./button"
 export {default as Form} from "./form"
 export {default as FormControl} from "./form/form-control"
