48d6bf230381e75a693f5cb7fc642c3daf9c4d18
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-08 21:58:56 +0200

    New: Enhance through adding `CardContent`

---
+++ components/ui/card/card-content.tsx
@@ -0,0 +1,10 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {HTMLAttributes, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const CardContent = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({className, ...props}, ref) => <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+)
+
+CardContent.displayName = "CardContent"
+export default CardContent
