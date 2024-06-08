68a0f6ca1ce55ea3da588a3a089a75fa9085c46f
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-08 21:48:34 +0200

    New: Enhance through adding `Card`

---
+++ components/ui/card/index.tsx
@@ -0,0 +1,16 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {HTMLAttributes, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
+  ({className, ...props}, ref) => (
+    <div
+      ref={ref}
+      className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
+      {...props}
+    />
+  )
+)
+
+Card.displayName = "Card"
+export default Card
