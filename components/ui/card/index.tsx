35b783a3296666942e18137495f6146de2f8dcb2
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-13 23:17:14 +0200

    Update: Enhance through applying `tailwindcss` theme to `Button`

--- components/ui/card/index.tsx
+++ components/ui/card/index.tsx
@@ -1,15 +1,9 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 import {HTMLAttributes, forwardRef} from "react"
 import {cn} from "../../../lib/utils"
 
 const Card = forwardRef<HTMLDivElement, HTMLAttributes<HTMLDivElement>>(
-  ({className, ...props}, ref) => (
-    <div
-      ref={ref}
-      className={cn("rounded-lg border bg-card text-card-foreground shadow-sm", className)}
-      {...props}
-    />
-  )
+  ({className, ...props}, ref) => <div ref={ref} className={cn("shc-card", className)} {...props} />
 )
 
 Card.displayName = "Card"
