5fb22715421e852b78da9693d5e71bef18a9f650
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-13 21:06:19 +0200

    New: Enhance through adding `ButtonExample`

---
+++ examples/button.tsx
@@ -0,0 +1,8 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Button} from "../components/ui"
+
+const ButtonExample = function ButtonExample() {
+  return <Button>Button</Button>
+}
+
+export default ButtonExample
--- examples/index.ts
+++ examples/index.ts
@@ -1,3 +1,4 @@
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
+export {default as ButtonExample} from "./button"
 export {default as FormBasic} from "./forms/basic"
-export default {}
+export {default as TRPCBasic} from "./trpc/basic"
