65f025db3f442259f4fb2f945f8b55d4a55086a8
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 16:11:09 +0200

    New: Enhance through adding `SheetFooter`

---
+++ components/ui/sheet/sheet-footer.tsx
@@ -0,0 +1,10 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {HTMLAttributes} from "react"
+import {cn} from "../../../lib/utils"
+
+const SheetFooter = function SheetFooter({className, ...props}: HTMLAttributes<HTMLDivElement>) {
+  return <div className={cn("shc-sheet-footer-base", className)} {...props} />
+}
+
+SheetFooter.displayName = "SheetFooter"
+export default SheetFooter
