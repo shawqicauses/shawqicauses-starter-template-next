bf499f77ed650f455f9d2a0163272fda26e6f589
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 20:33:01 +0200

    Fix: Enhance through adding `DialogContent` missing content

--- components/ui/dialog/dialog-content.tsx
+++ components/ui/dialog/dialog-content.tsx
@@ -1,20 +1,31 @@
 "use client"
 
-// DONE REVIEWING: GITHUB COMMIT
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 
 import * as DialogPrimitive from "@radix-ui/react-dialog"
+import {X} from "lucide-react"
 import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {DialogClose, DialogPortal} from "."
 import {cn} from "../../../lib/utils"
+import DialogOverlay from "./dialog-overlay"
 
 const DialogContent = forwardRef<
   ElementRef<typeof DialogPrimitive.Content>,
   ComponentPropsWithoutRef<typeof DialogPrimitive.Content>
->(({className, ...props}, ref) => (
-  <DialogPrimitive.Content
-    ref={ref}
-    className={cn("shc-dialog-content-base", className)}
-    {...props}
-  />
+>(({children, className, ...props}, ref) => (
+  <DialogPortal>
+    <DialogOverlay />
+    <DialogPrimitive.Content
+      ref={ref}
+      className={cn("shc-dialog-content-base", className)}
+      {...props}>
+      {children}
+      <DialogClose className="shc-dialog-close-base">
+        <span className="sr-only">Close</span>
+        <X aria-hidden="true" className="h-4 w-4" />
+      </DialogClose>
+    </DialogPrimitive.Content>
+  </DialogPortal>
 ))
 
 DialogContent.displayName = DialogPrimitive.Content.displayName
