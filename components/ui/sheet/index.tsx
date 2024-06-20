088e2a0c3e7818695d290f16eae3e3327e7b2c20
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 13:54:03 +0200

    New: Enhance through adding `Sheet`

---
+++ components/ui/sheet/index.tsx
@@ -0,0 +1,8 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as SheetPrimitive from "@radix-ui/react-dialog"
+
+const Sheet = SheetPrimitive.Root
+export const SheetPortal = SheetPrimitive.Portal
+export const SheetTrigger = SheetPrimitive.Trigger
+export const SheetClose = SheetPrimitive.Close
+export default Sheet
