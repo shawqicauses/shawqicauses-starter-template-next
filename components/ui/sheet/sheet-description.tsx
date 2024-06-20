875b1208e5e09e25f647bc0ecd2accb407d85a20
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 15:42:50 +0200

    New: Enhance through adding `SheetDescription`

---
+++ components/ui/sheet/sheet-description.tsx
@@ -0,0 +1,18 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as SheetPrimitive from "@radix-ui/react-dialog"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const SheetDescription = forwardRef<
+  ElementRef<typeof SheetPrimitive.Description>,
+  ComponentPropsWithoutRef<typeof SheetPrimitive.Description>
+>(({className, ...props}, ref) => (
+  <SheetPrimitive.Description
+    ref={ref}
+    className={cn("shc-sheet-description-base", className)}
+    {...props}
+  />
+))
+
+SheetDescription.displayName = SheetPrimitive.Description.displayName
+export default SheetDescription
