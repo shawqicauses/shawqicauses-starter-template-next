a1797c1bcec5e26b8a73bdf7aba55f164c7910d6
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 15:45:13 +0200

    New: Enhance through adding `SheetTitle`

---
+++ components/ui/sheet/sheet-title.tsx
@@ -0,0 +1,14 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as SheetPrimitive from "@radix-ui/react-dialog"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const SheetTitle = forwardRef<
+  ElementRef<typeof SheetPrimitive.Title>,
+  ComponentPropsWithoutRef<typeof SheetPrimitive.Title>
+>(({className, ...props}, ref) => (
+  <SheetPrimitive.Title ref={ref} className={cn("shc-sheet-title-base", className)} {...props} />
+))
+
+SheetTitle.displayName = SheetPrimitive.Title.displayName
+export default SheetTitle
