76798385d93c89797cf83225be4de049af5e6168
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 13:46:34 +0200

    Fix: Enhance through adding `use client` to missing components

--- components/ui/form/form-item.tsx
+++ components/ui/form/form-item.tsx
@@ -1,4 +1,6 @@
-// DONE REVIEWING: GITHUB COMMIT
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT 1️⃣
 
 import {HTMLAttributes, createContext, forwardRef, useId, useMemo} from "react"
 import {cn} from "../../../lib/utils"
