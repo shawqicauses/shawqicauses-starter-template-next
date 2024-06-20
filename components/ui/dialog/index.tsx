0b72fb7abb7eabfb782e717aa66af344f2f27391
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-20 17:38:07 +0200

    New: Enhance through adding `Dialog`

---
+++ components/ui/dialog/index.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT
+
+import * as DialogPrimitive from "@radix-ui/react-dialog"
+
+const Dialog = DialogPrimitive.Root
+export const DialogPortal = DialogPrimitive.Portal
+export const DialogTrigger = DialogPrimitive.Trigger
+export const DialogClose = DialogPrimitive.Close
+export default Dialog
