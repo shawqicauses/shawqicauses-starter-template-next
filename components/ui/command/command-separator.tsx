9853e0192e393e2e00fbc3aea665de9dc062792d
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-23 21:41:00 +0200

    New: Enhance through adding `CommandSeparator`

---
+++ components/ui/command/command-separator.tsx
@@ -0,0 +1,18 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Command as CommandPrimitive} from "cmdk"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const CommandSeparator = forwardRef<
+  ElementRef<typeof CommandPrimitive.Separator>,
+  ComponentPropsWithoutRef<typeof CommandPrimitive.Separator>
+>(({className, ...props}, ref) => (
+  <CommandPrimitive.Separator
+    ref={ref}
+    className={cn("shc-command-separator", className)}
+    {...props}
+  />
+))
+
+CommandSeparator.displayName = CommandPrimitive.Separator.displayName
+export default CommandSeparator
