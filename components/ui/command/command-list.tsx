33fbab7e6cc52d500485f381b73a757b1c929964
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-23 19:16:09 +0200

    New: Enhance through adding `CommandList`

---
+++ components/ui/command/command-list.tsx
@@ -0,0 +1,14 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Command as CommandPrimitive} from "cmdk"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const CommandList = forwardRef<
+  ElementRef<typeof CommandPrimitive.List>,
+  ComponentPropsWithoutRef<typeof CommandPrimitive.List>
+>(({className, ...props}, ref) => (
+  <CommandPrimitive.List ref={ref} className={cn("shc-command-list", className)} {...props} />
+))
+
+CommandList.displayName = CommandPrimitive.List.displayName
+export default CommandList
