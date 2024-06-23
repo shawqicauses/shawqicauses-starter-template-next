9c93ddaec1cb69da755f988421731648bbeb8f32
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-23 21:56:45 +0200

    New: Enhance through adding `CommandItem`

---
+++ components/ui/command/command-item.tsx
@@ -0,0 +1,14 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Command as CommandPrimitive} from "cmdk"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const CommandItem = forwardRef<
+  ElementRef<typeof CommandPrimitive.Item>,
+  ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
+>(({className, ...props}, ref) => (
+  <CommandPrimitive.Item ref={ref} className={cn("shc-command-item", className)} {...props} />
+))
+
+CommandItem.displayName = CommandPrimitive.Item.displayName
+export default CommandItem
