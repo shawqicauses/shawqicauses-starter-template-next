780f56e0bdf877da1b32dbe542abfe91acc5abbc
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-23 21:04:54 +0200

    New: Enhance through adding `CommandGroup`

---
+++ components/ui/command/command-group.tsx
@@ -0,0 +1,14 @@
+// DONE REVIEWING: GITHUB COMMIT
+import {Command as CommandPrimitive} from "cmdk"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const CommandGroup = forwardRef<
+  ElementRef<typeof CommandPrimitive.Group>,
+  ComponentPropsWithoutRef<typeof CommandPrimitive.Group>
+>(({className, ...props}, ref) => (
+  <CommandPrimitive.Group ref={ref} className={cn("shc-command-group", className)} {...props} />
+))
+
+CommandGroup.displayName = CommandPrimitive.Group.displayName
+export default CommandGroup
