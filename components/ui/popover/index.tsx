b5aefa040cd5d09be09852555e6b0dac1cc1544a
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-24 14:43:26 +0200

    New: Enhance through adding `Popover`

---
+++ components/ui/popover/index.tsx
@@ -0,0 +1,7 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as PopoverPrimitive from "@radix-ui/react-popover"
+
+const Popover = PopoverPrimitive.Root
+export const PopoverTrigger = PopoverPrimitive.Trigger
+export const PopoverAnchor = PopoverPrimitive.Anchor
+export default Popover
