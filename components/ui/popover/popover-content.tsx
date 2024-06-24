d3812dcb72598970423574b83ed69256bbb5a427
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-06-24 15:15:37 +0200

    New: Enhance through adding `PopoverContent`

---
+++ components/ui/popover/popover-content.tsx
@@ -0,0 +1,22 @@
+// DONE REVIEWING: GITHUB COMMIT
+import * as PopoverPrimitive from "@radix-ui/react-popover"
+import {ComponentPropsWithoutRef, ElementRef, forwardRef} from "react"
+import {cn} from "../../../lib/utils"
+
+const PopoverContent = forwardRef<
+  ElementRef<typeof PopoverPrimitive.Content>,
+  ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({className, align = "center", sideOffset = 4, ...props}, ref) => (
+  <PopoverPrimitive.Portal>
+    <PopoverPrimitive.Content
+      ref={ref}
+      align={align}
+      sideOffset={sideOffset}
+      className={cn("shc-popover-content-base", className)}
+      {...props}
+    />
+  </PopoverPrimitive.Portal>
+))
+
+PopoverContent.displayName = PopoverPrimitive.Content.displayName
+export default PopoverContent
