7be19c03738371f107810824c75ad9a49a1aa23d
Shawqi Hatem - The Wolf <shawqicauses@outlook.com>
2024-07-01 15:40:45 +0200

    New: Enhance through adding `Breadcrumb`

---
+++ components/ui/breadcrumb/index.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+// DONE REVIEWING: GITHUB COMMIT
+
+import {ComponentPropsWithoutRef, ReactNode, forwardRef} from "react"
+
+const Breadcrumb = forwardRef<
+  HTMLElement,
+  ComponentPropsWithoutRef<"nav"> & {
+    separator?: ReactNode
+  }
+>(({...props}, ref) => <nav ref={ref} aria-label="breadcrumb" {...props} />)
+
+Breadcrumb.displayName = "Breadcrumb"
+export default Breadcrumb
